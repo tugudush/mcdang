931cb99a4dd202f6537058a4ee30fb8767a5d33a
rossel <rossel.dev@gmail.com>
2017-09-14 22:13:06 +0800

    Changed api url to live

--- src/providers/globalVars.ts
+++ src/providers/globalVars.ts
@@ -1,10 +1,10 @@
 import { Injectable } from '@angular/core';
 
-const URL = 'http://192.168.0.19/mcdang/';
-const API_URL = 'http://192.168.0.19/mcdang/api/index.php/';
+/*const URL = 'http://192.168.0.19/mcdang/';
+const API_URL = 'http://192.168.0.19/mcdang/api/index.php/';*/
 
-/*const URL = 'http://theshiftleft.com/mcdang/';
-const API_URL = 'http://theshiftleft.com/mcdang/api/index.php/';*/
+const URL = 'http://theshiftleft.com/mcdang/';
+const API_URL = 'http://theshiftleft.com/mcdang/api/index.php/';
 
 @Injectable()
 export class GlobalVars {
